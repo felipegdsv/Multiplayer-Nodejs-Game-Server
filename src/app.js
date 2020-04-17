85a93b5f272b045ff87aeebd02bf01f991dd5ebb
felipe vieira <felipegdsv@gmail.com>
2020-04-17 05:34:28 -0300

    player attacks

--- src/app.js
+++ src/app.js
@@ -34,6 +34,10 @@ var Entity = function(){
         self.y += self.spdY;
     }
 
+    self.getDistance = function(pt){
+        return Math.sqrt(Math.pow(self.x-pt.x, 2) + Math.pow(self.y-pt.y,2));
+    }
+
     return self;
 }
 
@@ -41,18 +45,30 @@ var Player = function(id){
 
     var self = Entity();
 
-    self.id            = id                                ;
-    self.number        =  "" + Math.floor(10*Math.random());
-    self.pressingRight = false                             ;
-    self.pressingLeft  = false                             ;
-    self.pressingUp    = false                             ;
-    self.pressingDown  = false                             ;
-    self.maxSpd        = 10                                ;
-    
+    self.id             = id                                ;
+    self.number         =  "" + Math.floor(10*Math.random());
+    self.pressingRight  = false                             ;
+    self.pressingLeft   = false                             ;
+    self.pressingUp     = false                             ;
+    self.pressingDown   = false                             ;
+    self.pressingAttack = false                             ;
+    self.mouseAngle     = 0                                 ;
+    self.maxSpd         = 10                                ;
+
     var super_update = self.update;
     self.update = function(){
         self.updateSpd();
         super_update();
+
+        if(self.pressingAttack){
+            self.shootBullet(self.mouseAngle);
+        }
+    }
+
+    self.shootBullet = function (angle) {
+        var b = Bullet(self.id, angle);
+        b.x = self.x;
+        b.y = self.y;
     }
 
     self.updateSpd = function(){
@@ -80,14 +96,18 @@ Player.list = {};
 Player.onConnect = function(socket){
     var player = Player(socket.id);
     socket.on('keyPress', function(data){
-        if(data.inputId == 'right')
+        if(data.inputId === 'right')
             player.pressingRight = data.state;
-        if(data.inputId == 'left')
+        if(data.inputId === 'left')
             player.pressingLeft = data.state;
-        if(data.inputId == 'up')
+        if(data.inputId === 'up')
             player.pressingUp = data.state;
-        if(data.inputId == 'down')
+        if(data.inputId === 'down')
             player.pressingDown = data.state;
+        if(data.inputId === 'attack')
+            player.pressingAttack = data.state;
+        if(data.inputId === 'mouseAngle')
+            player.mouseAngle = data.state;
     });
 }
 
@@ -109,14 +129,15 @@ Player.Update = function(){
     return pack;
 }
 
-var Bullet = function(angle){
+var Bullet = function(parent, angle){
 
     var self = Entity();
 
-    self.id   = Math.random                     ;
+    self.id   = Math.random()                   ;
     self.spdX = Math.cos(angle/180*Math.PI) * 10;
     self.spdY = Math.sin(angle/180*Math.PI) * 10;
     
+    self.parent = parent;
     self.timer = 0;
     self.toRemove = false;
 
@@ -125,6 +146,14 @@ var Bullet = function(angle){
         if(self.timer++ > 100)
             self.toRemove = true;
         super_update();
+
+        for(var i in Player.list){
+            var p = Player.list[i];
+            if(self.getDistance(p) < 32 && self.parent !== p.id){
+                //TODO handle collision
+                self.toRemove = true;
+            }
+        }
     }
 
     Bullet.list[self.id] = self;
@@ -134,18 +163,17 @@ var Bullet = function(angle){
 Bullet.list = {};
 Bullet.Update = function(){
 
-    if(Math.random() < 0.1){
-        Bullet(Math.random()*360);
-    }
-
     var pack = []
     for(var i in Bullet.list){
         var bullet = Bullet.list[i];
         bullet.update();
-        pack.push({
-            x:bullet.x,
-            y:bullet.y,
-        });
+        if(bullet.toRemove)
+            delete Bullet.list[i];
+        else
+            pack.push({
+                x:bullet.x,
+                y:bullet.y,
+            });
     }
     return pack;
 }
@@ -178,7 +206,7 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('evalServer', function(data){
         if(!DEBUG)
-            return null;
+            return;
 
         var res = eval(data);
         socket.emit('evalAnswer', res);
