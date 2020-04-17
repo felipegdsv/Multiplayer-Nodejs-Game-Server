268b3b51c893313e9c73b36420ba598578e999ca
felipe vieira <felipegdsv@gmail.com>
2020-04-17 04:35:03 -0300

    adding chat

--- src/app.js
+++ src/app.js
@@ -150,6 +150,7 @@ Bullet.Update = function(){
     return pack;
 }
 
+var DEBUG = true;
 var io = require('socket.io') (serv, {});
 
 io.sockets.on('connection', function(socket) {
@@ -168,7 +169,21 @@ io.sockets.on('connection', function(socket) {
         console.log('disconnection ' + socket.id);
     });
 
-    
+    socket.on('sendMsgToServer', function(data){
+        var playerName = ("" + socket.id).slice(2, 7);
+        for(var i in SOCKET_LIST){
+                SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
+        }
+    });
+
+    socket.on('evalServer', function(data){
+        if(!DEBUG)
+            return null;
+
+        var res = eval(data);
+        socket.emit('evalAnswer', res);
+    });
+
 });
 
 setInterval(function(){
